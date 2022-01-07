75a7ba407a19ecf1a36220f25cac815b048a442d
HTG-YT <39023054+HTG-YT@users.noreply.github.com>
2022-01-07 16:15:01 +0800

    add datetime

---
+++ model/src/datetime/error.rs
@@ -0,0 +1,28 @@
+//! Errors when parsing timestamps.
+
+use std::error::Error;
+
+use time::error::Error as ParseError;
+
+/// Reason that a timestamp in ISO 8601 format could not be parsed.
+#[derive(Debug)]
+pub struct TimestampParseError {
+    pub kind: TimestampParseErrorKind,
+    pub source: Option<Box<dyn Error + Send + Sync>>
+}
+
+impl TimestampParseError {
+    /// Create a new error from a `time::error::Error`.
+    pub(super) fn from_parse(source: ParseError) -> Self {
+        Self {
+            kind: TimestampParseErrorKind::ParseFailure,
+            source: Some(Box::new(source))
+        }
+    }
+}
+
+/// Type of `TimestampParseError` that occurred.
+#[derive(Debug)]
+pub enum TimestampParseErrorKind {
+    ParseFailure
+}
--- model/src/lib.rs
+++ model/src/lib.rs
@@ -2,4 +2,5 @@
 //!
 //! `gwilight_model` is a crate of models for the Guilded API.
 
+pub mod datetime;
 pub mod id;
