07d575e246715f81832e2eeccc8885886e59f347
HTG-YT <39023054+HTG-YT@users.noreply.github.com>
2022-04-27 16:26:28 +0800

    add embed objects

---
+++ model/src/messaging/embed.rs
@@ -0,0 +1,147 @@
+//! A chat message embed.
+
+use serde::{Deserialize, Serialize};
+
+use crate::datetime::Timestamp;
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct ChatMessageEmbed {
+    author: Option<ChatMessageEmbedAuthor>,
+    color: Option<u32>,
+    description: Option<String>,
+    fields: Option<Vec<ChatMessageEmbedField>>,
+    footer: Option<ChatMessageEmbedFooter>,
+    image: Option<ChatMessageEmbedImage>,
+    thumbnail: Option<ChatMessageEmbedThumbnail>,
+    timestamp: Option<Timestamp>,  // FIXME: should belong in footer object
+    title: Option<String>,
+    url: Option<String>,
+}
+
+impl ChatMessageEmbed {
+    pub fn author(&self) -> Option<ChatMessageEmbedAuthor> {
+        self.author.clone()
+    }
+
+    pub fn color(&self) -> Option<u32> {
+        self.color
+    }
+
+    pub fn description(&self) -> Option<&str> {
+        self.description.as_deref()
+    }
+
+    pub fn fields(&self) -> Option<Vec<ChatMessageEmbedField>> {
+        self.fields.clone()
+    }
+
+    pub fn footer(&self) -> Option<ChatMessageEmbedFooter> {
+        self.footer.clone()
+    }
+
+    pub fn image(&self) -> Option<ChatMessageEmbedImage> {
+        self.image.clone()
+    }
+
+    pub fn thumbnail(&self) -> Option<ChatMessageEmbedThumbnail> {
+        self.thumbnail.clone()
+    }
+
+    pub fn timestamp(&self) -> Option<Timestamp> {
+        self.timestamp
+    }
+
+    pub fn title(&self) -> Option<&str> {
+        self.title.as_deref()
+    }
+
+    pub fn url(&self) -> Option<&str> {
+        self.url.as_deref()
+    }
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct ChatMessageEmbedAuthor {
+    icon_url: Option<String>,
+    name: Option<String>,
+    url: Option<String>,
+}
+
+impl ChatMessageEmbedAuthor {
+    pub fn icon_url(&self) -> Option<&str> {
+        self.icon_url.as_deref()
+    }
+
+    pub fn name(&self) -> Option<&str> {
+        self.name.as_deref()
+    }
+
+    pub fn url(&self) -> Option<&str> {
+        self.url.as_deref()
+    }
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct ChatMessageEmbedField {
+    inline: Option<bool>,
+    name: String,
+    value: String,
+}
+
+impl ChatMessageEmbedField {
+    pub fn inline(&self) -> Option<bool> {
+        self.inline
+    }
+
+    pub fn name(&self) -> &str {
+        self.name.as_ref()
+    }
+
+    pub fn value(&self) -> &str {
+        self.value.as_ref()
+    }
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct ChatMessageEmbedFooter {
+    icon_url: Option<String>,
+    text: String,
+}
+
+impl ChatMessageEmbedFooter {
+    pub fn icon_url(&self) -> Option<&str> {
+        self.icon_url.as_deref()
+    }
+
+    pub fn text(&self) -> &str {
+        self.text.as_ref()
+    }
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct ChatMessageEmbedImage {
+    url: Option<String>,
+}
+
+impl ChatMessageEmbedImage {
+    pub fn url(&self) -> Option<&str> {
+        self.url.as_deref()
+    }
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct ChatMessageEmbedThumbnail {
+    url: Option<String>,
+}
+
+impl ChatMessageEmbedThumbnail {
+    pub fn url(&self) -> Option<&str> {
+        self.url.as_deref()
+    }
+}
--- model/src/messaging/mod.rs
+++ model/src/messaging/mod.rs
@@ -7,8 +7,8 @@ use crate::id::{
     marker::{ChannelMarker, MessageMarker, ServerMarker, UserMarker, WebhookMarker},
     Id,
 };
-
 pub mod deleted_message;
+pub mod embed;
 
 /// Represents a chat message.
 #[derive(Clone, Debug, Deserialize, Serialize)]
@@ -19,6 +19,7 @@ pub struct ChatMessage {
     created_at: Timestamp,
     created_by: Id<UserMarker>,
     created_by_webhook_id: Option<Id<WebhookMarker>>,
+    embeds: Option<Vec<embed::ChatMessageEmbed>>,
     id: Id<MessageMarker>,
     is_private: Option<bool>,
     reply_message_ids: Option<Vec<Id<MessageMarker>>>,
@@ -46,6 +47,10 @@ impl ChatMessage {
 
     pub fn created_by_webhook_id(&self) -> Option<Id<WebhookMarker>> {
         self.created_by_webhook_id.clone()
+    }
+
+    pub fn embeds(&self) -> Option<Vec<embed::ChatMessageEmbed>> {
+        self.embeds.clone()
     }
 
     pub fn id(&self) -> Id<MessageMarker> {
