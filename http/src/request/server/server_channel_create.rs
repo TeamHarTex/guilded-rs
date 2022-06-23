2fadd8b259fa2198061d0fabbb220a4b22de31a0
HTG-YT <39023054+HTG-YT@users.noreply.github.com>
2022-06-23 20:24:58 +0800

    validation: add channel topic validation

--- http/src/request/server/server_channel_create.rs
+++ http/src/request/server/server_channel_create.rs
@@ -1,6 +1,7 @@
 use crate::client::Client;
-use guilded_model::channel::ServerChannelType;
 
+use guilded_model::channel::ServerChannelType;
+use guilded_validation::channel::{self, ChannelValidationError};
 use serde::Serialize;
 
 #[must_use = "requests must be configured and executed"]
@@ -9,8 +10,29 @@ pub struct ServerChannelCreate<'a> {
     fields: ServerChannelCreateFields<'a>,
 }
 
+impl<'a> ServerChannelCreate<'a> {
+    pub(crate) fn new(
+        client: &'a Client,
+        name: &'a str,
+        r#type: ServerChannelType,
+    ) -> Result<Self, ChannelValidationError> {
+        channel::validate_name_length(name)?;
+
+        Ok(Self {
+            client,
+            fields: ServerChannelCreateFields {
+                name,
+                topic: None,
+                r#type,
+            }
+        })
+    }
+}
+
 #[derive(Serialize)]
 struct ServerChannelCreateFields<'a> {
     name: &'a str,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    topic: Option<&'a str>,
     r#type: ServerChannelType,
 }
--- validation/src/channel.rs
+++ validation/src/channel.rs
@@ -12,6 +12,9 @@ impl Display for ChannelValidationError {
             ChannelValidationErrorType::InvalidNameLength => {
                 f.write_str("invalid length of channel name")
             }
+            ChannelValidationErrorType::InvalidTopicLength => {
+                f.write_str("invalid length of channel topic")
+            }
         }
     }
 }
@@ -22,10 +25,14 @@ impl Error for ChannelValidationError {}
 #[non_exhaustive]
 pub enum ChannelValidationErrorType {
     InvalidNameLength,
+    InvalidTopicLength,
 }
 
 pub const CHANNEL_NAME_MIN_LENGTH: usize = 1;
 pub const CHANNEL_NAME_MAX_LENGTH: usize = 100;
+
+pub const CHANNEL_TOPIC_MIN_LENGTH: usize = 1;
+pub const CHANNEL_TOPIC_MAX_LENGTH: usize = 512;
 
 pub fn validate_name_length(name: impl AsRef<str>) -> Result<(), ChannelValidationError> {
     let length = name.as_ref().chars().count();
@@ -36,5 +43,17 @@ pub fn validate_name_length(name: impl AsRef<str>) -> Result<(), ChannelValidati
 
     Err(ChannelValidationError {
         r#type: ChannelValidationErrorType::InvalidNameLength,
+    })
+}
+
+pub fn validate_topic_length(topic: impl AsRef<str>) -> Result<(), ChannelValidationError> {
+    let length = topic.as_ref().chars().count();
+
+    if (CHANNEL_TOPIC_MIN_LENGTH..=CHANNEL_TOPIC_MAX_LENGTH).contains(&length) {
+        return Ok(());
+    }
+
+    Err(ChannelValidationError {
+        r#type: ChannelValidationErrorType::InvalidTopicLength,
     })
 }
