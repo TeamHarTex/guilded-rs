08269028e2ab317995cbbc3d12194221fd172c05
HTG-YT <39023054+HTG-YT@users.noreply.github.com>
2022-06-29 19:10:20 +0800

    util: complete constructor for ChatEmbedFooterBuilder

--- util/src/builder/embed/footer.rs
+++ util/src/builder/embed/footer.rs
@@ -1,12 +1,18 @@
 use guilded_model::messaging::embed::ChatEmbedFooter;
-use guilded_validation::embed::EmbedValidationError;
+use guilded_validation::embed::footer::{self, EmbedFooterValidationError};
 
 #[derive(Clone, Debug, Eq, PartialEq)]
 #[must_use = "must be built into an embed footer"]
 pub struct ChatEmbedFooterBuilder(ChatEmbedFooter);
 
 impl ChatEmbedFooterBuilder {
-    pub fn new(text: impl Into<String>) -> Result<Self, EmbedValidationError> {
-        todo!()
+    pub fn new(text: impl Into<String>) -> Result<Self, EmbedFooterValidationError> {
+        let text = text.into();
+        footer::validate_footer_text_length(&text)?;
+
+        Ok(Self(ChatEmbedFooter {
+            icon_url: None,
+            text,
+        }))
     }
 }
