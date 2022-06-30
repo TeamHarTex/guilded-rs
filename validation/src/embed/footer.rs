65e9e2a94affb964636956660c6f6b9c822f406a
HTG-YT <39023054+HTG-YT@users.noreply.github.com>
2022-06-30 22:39:41 +0800

    validation: bump and add validationfor footer icon url

--- validation/src/embed/footer.rs
+++ validation/src/embed/footer.rs
@@ -11,6 +11,9 @@ pub struct EmbedFooterValidationError {
 impl Display for EmbedFooterValidationError {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         match self.r#type {
+            EmbedFooterValidationErrorType::InvalidIconUrlLength => {
+                f.write_str("invalid length of embed footer icon url")
+            }
             EmbedFooterValidationErrorType::InvalidTextLength => {
                 f.write_str("invalid length of embed footer text")
             }
@@ -23,10 +26,26 @@ impl Error for EmbedFooterValidationError {}
 #[derive(Debug)]
 #[non_exhaustive]
 pub enum EmbedFooterValidationErrorType {
+    InvalidIconUrlLength,
     InvalidTextLength,
 }
 
+pub const EMBED_FOOTER_ICON_URL_MAX_LENGTH: usize = 1024;
 pub const EMBED_FOOTER_TEXT_MAX_LENGTH: usize = 2048;
+
+pub fn validate_footer_icon_url_length(
+    footer_icon_url: impl AsRef<str>,
+) -> ValidationResult<EmbedFooterValidationError> {
+    let length = footer_icon_url.as_ref().chars().count();
+
+    if length <= EMBED_FOOTER_ICON_URL_MAX_LENGTH {
+        return Ok(());
+    }
+
+    Err(EmbedFooterValidationError {
+        r#type: EmbedFooterValidationErrorType::InvalidIconUrlLength,
+    })
+}
 
 pub fn validate_footer_text_length(
     footer_text: impl AsRef<str>,
