f480f43dbc97dd0fd859b02c6c1b537f65de906e
HTGAzureX1212 <39023054+HTG-YT@users.noreply.github.com>
2022-07-28 13:04:57 +0800

    update forum topic

--- model/src/forum/topic/mod.rs
+++ model/src/forum/topic/mod.rs
@@ -2,22 +2,27 @@
 
 use serde::{Deserialize, Serialize};
 
+use crate::channel::mentions::Mentions;
 use crate::datetime::Timestamp;
 use crate::id::{
     marker::{ChannelMarker, ForumThreadMarker, ServerMarker, UserMarker, WebhookMarker},
     Id,
 };
 
+pub mod partial;
+
 /// Represents a forum topic.
 #[derive(Clone, Debug, Deserialize, Serialize)]
 #[serde(rename_all = "camelCase")]
 pub struct ForumTopic {
+    pub bumped_at: Option<Timestamp>,
     pub channel_id: Id<ChannelMarker>,
     pub content: Option<String>,
     pub created_at: Timestamp,
     pub created_by: Id<UserMarker>,
     pub created_by_webhook_id: Option<Id<WebhookMarker>>,
     pub id: Id<ForumThreadMarker>,
+    pub mentions: Option<Mentions>
     pub server_id: Id<ServerMarker>,
     pub title: Option<String>,
     pub updated_at: Option<Timestamp>,
