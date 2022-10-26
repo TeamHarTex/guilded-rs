40e2927254a9d42a2caa5d77c6db7964e27413f6
HTGAzureX1212 <39023054+HTG-YT@users.noreply.github.com>
2022-10-26 15:36:15 +0800

    finish validation for fields

--- validation/src/embed/field.rs
+++ validation/src/embed/field.rs
@@ -11,6 +11,12 @@ pub struct ChatEmbedFieldValidationError {
 impl Display for ChatEmbedFieldValidationError {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         match self.r#type {
+            ChatEmbedFieldValidationErrorType::InvalidNameLength => {
+                f.write_str("invalid length of embed field name")
+            }
+            ChatEmbedFieldValidationErrorType::InvalidValueLength => {
+                f.write_str("invalid length of embed field value")
+            }
         }
     }
 }
@@ -20,4 +26,37 @@ impl Error for ChatEmbedFieldValidationError {}
 #[derive(Debug)]
 #[non_exhaustive]
 pub enum ChatEmbedFieldValidationErrorType {
+    InvalidNameLength,
+    InvalidValueLength,
+}
+
+pub const EMBED_FIELD_NAME_MAX_LENGTH: usize = 256;
+pub const EMBED_FIELD_VALUE_MAX_LENGTH: usize = 1024;
+
+pub fn validate_field_name_length(
+    name: impl AsRef<str>,
+) -> ValidationResult<ChatEmbedFieldValidationError> {
+    let length = name.as_ref().chars().count();
+
+    if length <= EMBED_FIELD_NAME_MAX_LENGTH {
+        return Ok(());
+    }
+
+    Err(ChatEmbedFieldValidationError {
+        r#type: ChatEmbedFieldValidationErrorType::InvalidNameLength,
+    })
+}
+
+pub fn validate_field_value_length(
+    value: impl AsRef<str>,
+) -> ValidationResult<ChatEmbedFieldValidationError> {
+    let length = value.as_ref().chars().count();
+
+    if length <= EMBED_FIELD_VALUE_MAX_LENGTH {
+        return Ok(());
+    }
+
+    Err(ChatEmbedFieldValidationError {
+        r#type: ChatEmbedFieldValidationErrorType::InvalidValueLength,
+    })
 }
